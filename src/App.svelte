ca8b6808df530c7ddd6486d552bb34ecda9d9499
Hariss Ali Gills <harissgills@gmail.com>
2022-08-31 23:30:51 +0300

    Added dynamic behaviour

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,12 @@
+<!-- App.svelte -->
 <script>
   import Todos from './components/Todos.svelte'
+
+  let todos = [
+    { id: 1, name: 'Create a Svelte starter app', completed: true },
+    { id: 2, name: 'Create your first component', completed: true },
+    { id: 3, name: 'Complete the rest of the tutorial', completed: false }
+  ]
 </script>
 
-<Todos />
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<Todos {todos} />
