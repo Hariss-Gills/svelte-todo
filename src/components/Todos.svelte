8a59f43bb9bbc9fe11c2f2ae837b448c0511ac19
Hariss Ali Gills <harissgills@gmail.com>
2022-09-04 17:23:21 +0300

    Ported to Typescript

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,93 +1,81 @@
 <!-- components/Todos.svelte -->
-<script>
-  export let todos = []
-  
-  let newTodoName = ''
-  $: newTodoId = totalTodos ? Math.max(...todos.map(t => t.id)) + 1 : 1
+<script lang='ts'>
+  import FilterButton from './FilterButton.svelte'
+  import Todo from './Todo.svelte'
+  import MoreActions from './MoreActions.svelte'
+  import NewTodo from './NewTodo.svelte'
+  import TodosStatus from './TodosStatus.svelte'
+  import { alert } from '../stores'
 
-  $: totalTodos = todos.length
-  $: completedTodos = todos.filter(todo => todo.completed).length
+  import { Filter } from '../types/filter.enum'
 
-  function removeTodo(todo) {
+  import type { TodoType } from '../types/todo.type'
+
+  export let todos: TodoType[] = []
+
+  let todosStatus: TodosStatus                   // reference to TodosStatus instance
+
+  $: newTodoId = todos.length > 0 ? Math.max(...todos.map(t => t.id)) + 1 : 1
+
+  function addTodo(name: string) {
+    todos = [...todos, { id: newTodoId, name, completed: false }]
+    $alert = `Todo '${name}' has been added`
+  }
+
+  function removeTodo(todo: TodoType) {
     todos = todos.filter(t => t.id !== todo.id)
+    todosStatus.focus()             // give focus to status heading
+    $alert = `Todo '${todo.name}' has been deleted`
   }
 
-  function addTodo() {
-    todos = [...todos, { id: newTodoId, name: newTodoName, completed: false }]
-    newTodoName = ''
+  function updateTodo(todo: TodoType) {
+    const i = todos.findIndex(t => t.id === todo.id)
+    if (todos[i].name !== todo.name)            $alert = `todo '${todos[i].name}' has been renamed to '${todo.name}'`
+    if (todos[i].completed !== todo.completed)  $alert = `todo '${todos[i].name}' marked as ${todo.completed ? 'completed' : 'active'}`
+    todos[i] = { ...todos[i], ...todo }
   }
 
-  let filter = 'all'
-  const filterTodos = (filter, todos) => 
-    filter === 'active' ? todos.filter(t => !t.completed) :
-    filter === 'completed' ? todos.filter(t => t.completed) : 
+  let filter: Filter = Filter.ALL
+  const filterTodos = (filter: Filter, todos: TodoType[]) => 
+    filter === Filter.ACTIVE ? todos.filter(t => !t.completed) :
+    filter === Filter.COMPLETED ? todos.filter(t => t.completed) : 
     todos
 
+  $: {
+    if (filter === Filter.ALL)               $alert = 'Browsing all todos'
+    else if (filter === Filter.ACTIVE)       $alert = 'Browsing active todos'
+    else if (filter === Filter.COMPLETED)    $alert = 'Browsing completed todos'
+  }
+
+  const checkAllTodos = (completed: boolean) => {
+    todos = todos.map(t => ({...t, completed}))
+    $alert = `${completed ? 'Checked' : 'Unchecked'} ${todos.length} todos`
+  }
+  const removeCompletedTodos = () => {
+    $alert = `Removed ${todos.filter(t => t.completed).length} todos`
+    todos = todos.filter(t => !t.completed)
+  }
 </script>
 
-<!-- Todos.svelte -->
 <div class="todoapp stack-large">
 
   <!-- NewTodo -->
-  <form on:submit|preventDefault={addTodo}>
-    <h2 class="label-wrapper">
-      <label for="todo-0" class="label__lg">
-        What needs to be done?
-      </label>
-    </h2>
-    <input bind:value={newTodoName} type="text" id="todo-0" autocomplete="off" class="input input__lg" />
-    <button type="submit" disabled="" class="btn btn__primary btn__lg">
-      Add
-    </button>
-  </form>
+  <NewTodo autofocus on:addTodo={e => addTodo(e.detail)} />
 
   <!-- Filter -->
-  <div class="filters btn-group stack-exception">
-    <button class="btn toggle-btn" class:btn__primary={filter === 'all'} aria-pressed={filter === 'all'} on:click={()=> filter = 'all'} >
-      <span class="visually-hidden">Show</span>
-      <span>All</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button class="btn toggle-btn" class:btn__primary={filter === 'active'} aria-pressed={filter === 'active'} on:click={()=> filter = 'active'} >
-      <span class="visually-hidden">Show</span>
-      <span>Active</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button class="btn toggle-btn" class:btn__primary={filter === 'completed'} aria-pressed={filter === 'completed'} on:click={()=> filter = 'completed'} >
-      <span class="visually-hidden">Show</span>    
-      <span>Completed</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-  </div>
+  <FilterButton bind:filter />
 
   <!-- TodosStatus -->
-  <h2 id="list-heading">{completedTodos} out of {totalTodos} items completed</h2>
+  <TodosStatus bind:this={todosStatus} {todos} />
 
   <!-- Todos -->
   <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
   {#each filterTodos(filter, todos) as todo (todo.id)}
     <li class="todo">
-      <div class="stack-small">
-        <div class="c-cb">
-            <input type="checkbox" id="todo-{todo.id}" 
-              on:click={() => todo.completed = !todo.completed}
-              checked={todo.completed}
-            />          
-            <label for="todo-{todo.id}" class="todo-label">
-            {todo.name}
-          </label>
-        </div>
-        <div class="btn-group">
-          <button type="button" class="btn">
-            Edit <span class="visually-hidden">{todo.name}</span>
-          </button>
-          <button type="button" class="btn btn__danger"
-            on:click={() => removeTodo(todo)}
-          >
-            Delete <span class="visually-hidden">{todo.name}</span>
-          </button>
-        </div>
-      </div>
+      <Todo {todo}
+        on:update={e => updateTodo(e.detail)}
+        on:remove={e => removeTodo(e.detail)}
+      />
     </li>
   {:else}
     <li>Nothing to do here!</li>
@@ -97,9 +85,9 @@
   <hr />
 
   <!-- MoreActions -->
-  <div class="btn-group">
-    <button type="button" class="btn btn__primary">Check all</button>
-    <button type="button" class="btn btn__primary">Remove completed</button>
-  </div>
+  <MoreActions {todos}
+    on:checkAll={e => checkAllTodos(e.detail)}
+    on:removeCompleted={removeCompletedTodos}
+  />
 
 </div>
